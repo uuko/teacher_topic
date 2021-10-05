5948a56d92752f00b9c6798dba4edc2801ea7a28
uuko <omkkqq@gmail.com>
2021-10-05 16:52:28 +0800

    "師資陣容(第二層) / 師資陣容第一層UI"

---
+++ app/src/main/java/com/example/linteacher/ui/main/teacherline/tchsencondline/adapter/AcadeMicEventsAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.linteacher.ui.main.teacherline.tchsencondline.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.linteacher.api.pojo.teacherline.Event
+import com.example.linteacher.databinding.ItemMainAcademiceventsBinding
+
+class AcadeMicEventsAdapter(
+    var list: List<Event>,
+) : RecyclerView.Adapter<AcadeMicEventsAdapter.ViewHolder>() {
+
+    fun setDataList(list: MutableList<Event>) {
+        this.list = list
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val headerBinding: ItemMainAcademiceventsBinding =
+            ItemMainAcademiceventsBinding.inflate(layoutInflater, parent, false)
+        return ViewHolder(headerBinding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class ViewHolder(val binding: ItemMainAcademiceventsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(response: Event) {
+
+            binding.eveName.text = response.eveName
+            binding.eveCategory.text = response.eveCategory
+            binding.eveSort.text = response.eveSort
+
+        }
+
+    }
+}
